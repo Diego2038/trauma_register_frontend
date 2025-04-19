d2625cd7ffe209010b26e4ffb6398c31f72b021f
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2025-04-19 11:24:04 -0500

    V0.3.0 Update: Login view and hospital image implemented

--- lib/presentation/views/login_view.dart
+++ lib/presentation/views/login_view.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:trauma_register_frontend/core/enums/custom_size.dart';
 import 'package:trauma_register_frontend/core/routes/app_router.dart';
+import 'package:trauma_register_frontend/core/themes/app_colors.dart';
+import 'package:trauma_register_frontend/core/themes/app_size_text.dart';
+import 'package:trauma_register_frontend/core/themes/app_text.dart';
 import 'package:trauma_register_frontend/data/services/navigation_service.dart';
 import 'package:trauma_register_frontend/presentation/providers/auth_provider.dart';
+import 'package:trauma_register_frontend/presentation/widgets/custom_button.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -15,10 +20,15 @@ class _LoginViewState extends State<LoginView> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _isLoading = false;
+  bool _isError = false;
 
-  void _login(BuildContext context) async {
+  Future<void> _login(BuildContext context) async {
+    if (_isLoading) return;
     final authProvider = Provider.of<AuthProvider>(context, listen: false);
-    setState(() => _isLoading = true);
+    setState(() {
+      _isLoading = true;
+      _isError = false;
+    });
 
     bool success = await authProvider.login(
       username: _emailController.text.trim(),
@@ -28,9 +38,7 @@ class _LoginViewState extends State<LoginView> {
     setState(() => _isLoading = false);
     print(success);
     if (!success) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Credenciales incorrectas.')),
-      );
+      setState(() => _isError = true);
       return;
     }
     NavigationService.navigateTo(AppRouter.home);
@@ -40,65 +48,167 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.white,
+        color: AppColors.base,
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Image.network(
-                      'https://scontent.fclo3-2.fna.fbcdn.net/v/t39.30808-6/465759821_1012204660940337_3699946573436549494_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=n2MrlImYo28Q7kNvgGfmQZr&_nc_oc=AdlcrKfCUg4fTYb2ljEqtlk2Xo8DqKxzP1wjBVv_8Ei4tyO5SZ0k48PIJ_zoR_qCA3E&_nc_zt=23&_nc_ht=scontent.fclo3-2.fna&_nc_gid=vWKo_JYSsKwWLI6_ImYwvw&oh=00_AYEOgrqFgWBDvEe_A5oDjADjkeXcYWiaQoKjXcWVXrTnUQ&oe=67E26C6D',
-                      fit: BoxFit.cover,
-                    ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                  color: AppColors.white,
+                  height: double.infinity,
+                  child: Image.asset(
+                    'assets/images/huv_simple_logo.jpg',
+                    fit: BoxFit.cover,
                   ),
                 ),
-                const SizedBox(width: 50),
-                Expanded(
-                  child: Card(
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(30.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Bienvenido',
-                            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 20),
-                          TextField(
-                            controller: _emailController,
-                            decoration: const InputDecoration(labelText: 'Correo electrónico'),
-                          ),
-                          const SizedBox(height: 10),
-                          TextField(
-                            controller: _passwordController,
-                            obscureText: true,
-                            decoration: const InputDecoration(labelText: 'Contraseña'),
-                          ),
-                          const SizedBox(height: 20),
-                          SizedBox(
-                            width: double.infinity,
-                            child: ElevatedButton(
-                              onPressed: _isLoading ? null : () => _login(context),
-                              child: _isLoading
-                                  ? const CircularProgressIndicator(color: Colors.white)
-                                  : const Text('Ingresar'),
-                            ),
-                          ),
-                        ],
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 0,
+                    left: 50,
+                    right: 50,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: double.infinity,
+                        height: 60,
+                        child: H1(
+                          text: "Inicio de sesión",
+                          color: AppColors.white,
+                        ),
                       ),
-                    ),
+                      const SizedBox(
+                        width: double.infinity,
+                        height: 20,
+                        child: H6(
+                          text: "Bienvenido al portal de registro de traumas",
+                          color: AppColors.grey200,
+                        ),
+                      ),
+                      const SizedBox(height: 37),
+                      const SizedBox(
+                        width: double.infinity,
+                        child: H4(
+                          text: "Usuario",
+                          color: AppColors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      customInput(
+                        controller: _emailController,
+                        hintText: "Ingrese su correo electrónico",
+                      ),
+                      const SizedBox(height: 37),
+                      const SizedBox(
+                        width: double.infinity,
+                        child: H4(
+                          text: "Contraseña",
+                          color: AppColors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      customInput(
+                        controller: _passwordController,
+                        hintText: "Ingrese su contraseña",
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 37),
+                      Center(
+                        child: Column(
+                          children: [
+                            CustomButton(
+                              size: CustomSize.h4,
+                              height: 34,
+                              width: 305,
+                              centerButtonContent: true,
+                              text: "Ingresar",
+                              onPressed: () async {
+                                await _login(
+                                    context.mounted ? context : context);
+                              },
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 15),
+                              child: SizedBox(
+                                height: 25,
+                                child: _isError
+                                    ? const H6(
+                                        text:
+                                            "Usuario o contraseña no válidas, por favor intente nuevamente",
+                                        color: AppColors.errorLogin,
+                                      )
+                                    : null,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget customInput({
+    required TextEditingController controller,
+    required String hintText,
+    bool obscureText = false,
+  }) {
+    const fontSize = AppSizeText.h4;
+    return Container(
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.25), 
+          offset: const Offset(4, 4),
+          blurRadius: 4,
+        ),
+      ]),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        style: const TextStyle(
+          fontSize: fontSize,
+          color: AppColors.black,
+          fontWeight: FontWeight.normal,
+        ),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: AppColors.white,
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            fontSize: fontSize,
+            color: AppColors.grey200,
+            fontWeight: FontWeight.w300,
+          ),
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.zero,
+            borderSide: BorderSide(color: AppColors.black, width: 0),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.zero,
+            borderSide: BorderSide(color: AppColors.black, width: 0),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.zero,
+            borderSide: BorderSide(color: AppColors.black, width: 0),
+          ),
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 15,
+            horizontal: 10,
           ),
         ),
       ),
