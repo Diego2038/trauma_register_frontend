816b67fa9b803bc4ce96ae5a2db2341623cdcb5e
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2025-09-09 22:42:37 -0500

    V0.35.1 Update: More space below the widget

--- lib/presentation/views/patient_management/patient_management_view.dart
+++ lib/presentation/views/patient_management/patient_management_view.dart
@@ -602,6 +602,7 @@ class _ContentDataPatient extends StatelessWidget {
           action: action,
           freeSize: freeSize,
         ),
+        const SizedBox(height: 85),
       ],
     );
   }
